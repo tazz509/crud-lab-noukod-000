a963807b40b4bdd5872663a93aa7c0805ff7285d
carly <carly@noukod.com>
2019-04-09 15:05:33 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
-import Review from './Review';
+import React, { Component } from 'react'
+import Review from './Review'
 
 class Reviews extends Component {
-  render() {
-    return (
-      <ul>
-        Reviews
-      </ul>
-    );
-  }
-};
 
-export default Reviews;
+  render() {
+  	
+    const { reviews, restaurantId, deleteReview } = this.props
+    const allReviews = reviews.filter(review => review.restaurantId === restaurantId)
+   
+    const restaurantReviews = allReviews.map(review =>
+									  <Review key={review.id}
+									   review={review}
+									   deleteReview={deleteReview}
+									  />
+	)
+    
+    return (
+        <ul>
+          {restaurantReviews}
+        </ul>
+    )
+  }
+}
+
+export default Reviews
