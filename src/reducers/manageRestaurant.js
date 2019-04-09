2f943c871247d715bea7004d0bdacd74757bc11a
carly <carly@noukod.com>
2019-04-09 15:07:33 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,44 @@
+import cuid from 'cuid'
 
-import cuid from 'cuid';
-export const cuidFn = cuid;
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: [],
+}, action) {
+  switch(action.type) {
 
-export default function manageRestaurants(state, action) {
+    case 'ADD_RESTAURANT':
+
+      const restaurant = { id: cuid(), text: action.text } 
+
+      return (
+        action.text !== '' ?
+        { ...state, restaurants:  [...state.restaurants, restaurant] } : 
+        state
+      )
+
+    case 'DELETE_RESTAURANT':
+
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
+      return { ...state, restaurants }
+
+    case 'ADD_REVIEW':
+     
+      const review = { id: cuid(), text: action.review.text, restaurantId: action.review.restaurantId }
+      
+      return (
+        action.review.text !== '' ?
+        { ...state, reviews: [...state.reviews, review] } :
+        state
+      )
+
+    case 'DELETE_REVIEW':
+      
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return { ...state, reviews }
+
+    default:
+
+      return state
+  }
 
 }
