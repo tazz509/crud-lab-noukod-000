19b9a753126c33d9574fac5e4df8edf37c7d38b8
carly <carly@noukod.com>
2019-04-09 15:04:18 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react'
+import Restaurant from './Restaurant'
 
-class Restaurants extends Component {
-  render() {
+const Restaurants = props => {
+
+	const restaurants = props.restaurants.map(restaurant => 
+						<Restaurant key={restaurant.id} 
+						 restaurant={restaurant} 
+						 deleteRestaurant={props.deleteRestaurant} 
+						/>
+	)
+  
     return(
       <ul>
-        Restaurants Component
+        {restaurants}
       </ul>
-    );
-  }
-};
+    )
+}
 
-export default Restaurants;
+export default Restaurants
